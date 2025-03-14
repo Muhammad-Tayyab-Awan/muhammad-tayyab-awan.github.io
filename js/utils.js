c5807b2c325a96a64c02fcb6d56c95dfd49b5051
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-03-14 19:16:05 +0500

    Added year display and scroll-triggered animation for photo section

--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,13 @@
+// Year Setting
 const year = document.getElementById("year");
 year.innerText = new Date().getFullYear();
+// Animation Handling
+const box = document.querySelector(".my-photo");
+function checkScroll() {
+  const rect = box.getBoundingClientRect();
+  if (rect.top < window.innerHeight * 0.8) {
+    box.classList.add("slide-down");
+  }
+}
+window.addEventListener("scroll", checkScroll);
+checkScroll();
