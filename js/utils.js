bf824ef7dd1e48a4fd8ad9e1981c42d7b6ad63c9
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-03-14 21:26:14 +0500

    Refactor animation handling to include additional elements for scroll-triggered effects

--- js/utils.js
+++ js/utils.js
@@ -2,11 +2,16 @@
 const year = document.getElementById("year");
 year.innerText = new Date().getFullYear();
 // Animation Handling
-const box = document.querySelector(".my-photo");
+const box1 = document.querySelector(".my-photo");
+const box2 = document.querySelector(".name");
 function checkScroll() {
-  const rect = box.getBoundingClientRect();
-  if (rect.top < window.innerHeight * 0.8) {
-    box.classList.add("slide-down");
+  const rect1 = box1.getBoundingClientRect();
+  if (rect1.top < window.innerHeight * 0.8) {
+    box1.classList.add("slide-down");
+  }
+  const rect2 = box2.getBoundingClientRect();
+  if (rect2.top < window.innerHeight * 0.8) {
+    box2.classList.add("slide-down");
   }
 }
 window.addEventListener("scroll", checkScroll);
